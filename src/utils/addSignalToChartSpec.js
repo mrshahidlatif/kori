41dadd7aa3696a6ab7ae33f6291f49337174a6d6
Nam Wook Kim <namwookkim@Nams-MacBook-Air.local>
2020-04-18 01:13:14 -0400

    improving single point linking

--- src/utils/addSignalToChartSpec.js
+++ src/utils/addSignalToChartSpec.js
@@ -1,12 +1,6 @@
-// import { getVariableNameFromScale } from "./VegaHelpers";
-function addSignalToChartSpec(spec) {
-  // console.log("Old Chart Specs:", oldChartSpecs);
+// TODO: support different encodings?
 
-  //I think this is not the right way to work with data cloning in Reactjs!
-  //soultion Source: https://stackoverflow.com/questions/55567386/react-cannot-add-property-x-object-is-not-extensible
-  // let spec = JSON.parse(JSON.stringify(oldChartSpecs));
-  // spec.width = 350;
-  // spec.height = 200;
+export default (spec)=>{
 
   // highlight
   spec.signals = [
@@ -17,131 +11,32 @@ function addSignalToChartSpec(spec) {
     }
   ];
 
-  // TODO: the following is temporary
-  // const fields = spec.scales.filter(scale=>scale.domain.field)
-  //   .map(scale=>scale.domain.field);
-    // ${fields.map(field=>`
-    //             
-    //           `).join('')}
-  // console.log('fields', fields);
   spec.marks.forEach(mark=>{
-    mark.encode.update = {
-      ...mark.encode.update,
-      fillOpacity:[
-        {
-          test:` 
-              highlight.enabled === false 
-              || indexof(highlight.data, datum[highlight.field])!=-1
-            `,
-          value: 1.0
-        },
-        {
-          value: 0.2
-        }
-      ]
-    }
+    addSignalToMark(mark);
   })
- 
-  // add mark update rules
-  
-  //Case Stack-Bar chart
-  // if (chartType === "stack-bar") {
-  //   const x = getVariableNameFromScale(spec, "x");
-  //   const y = getVariableNameFromScale(spec, "y");
-  //   const color = getVariableNameFromScale(spec, "color");
-
-  //   spec.marks[0].encode.update = {
-  //     ...spec.marks[0].encode.update,
-  //     fillOpacity: [
-  //       {
-  //         test:
-  //           "highlight.data[0] !== 'CLEAR' && (" +
-  //           "indexof(highlight.data,datum." +
-  //           x +
-  //           ") != -1 || (datum." +
-  //           y +
-  //           " > highlight.data[0] && datum." +
-  //           y +
-  //           " < highlight.data[1]) || indexof(highlight.data,datum." +
-  //           color +
-  //           ") != -1)",
-  //         value: 1.0
-  //       },
-  //       { value: 0.6 }
-  //     ]
-  //   };
-  // }
-  // // Case Simple Bar chart
-  // else if (chartType === "bar") {
-  //   const x = getVariableNameFromScale(spec, "xscale");
-  //   const y = getVariableNameFromScale(spec, "yscale");
-
-  //   spec.marks[0].encode.update = {
-  //     fill: { value: "steelblue" },
-  //     fillOpacity: [
-  //       {
-  //         test:
-  //           "highlight.data[0] !== 'CLEAR' && (" +
-  //           "indexof(highlight.data,datum." +
-  //           x +
-  //           ") >= 0 || (datum." +
-  //           y +
-  //           " > highlight.data[0] && datum." +
-  //           y +
-  //           " < highlight.data[1]))",
-  //         value: 1.0
-  //       },
-  //       { value: 0.6 }
-  //     ]
-  //   };
-  // }
-  // // Case Multi-Line Chart
-  // else if (chartType === "multi-line") {
-  //   const x = getVariableNameFromScale(spec, "x");
-  //   const y = getVariableNameFromScale(spec, "y");
-  //   const color = getVariableNameFromScale(spec, "color");
-
-  //   if (spec.marks[0].marks[0].encode.hasOwnProperty("update")) {
-  //     spec.marks[0].marks[0].encode.update.strokeOpacity = [
-  //       {
-  //         test:
-  //           "indexof(highlight.data,datum." +
-  //           x +
-  //           ") !== -1 || (datum." +
-  //           y +
-  //           " > highlight.data[0] && datum." +
-  //           y +
-  //           " < highlight.data[1]) || indexof(highlight.data,datum." +
-  //           color +
-  //           ") !== -1",
-  //         value: 1.0
-  //       },
-  //       { value: 0.6 }
-  //     ];
-  //   } else {
-  //     spec.marks[0].marks[0].encode.update = {
-  //       //TODO: BUG: When value is 0, it highlights both the lines!
-  //       strokeOpacity: [
-  //         {
-  //           test:
-  //             "indexof(highlight.data,datum." +
-  //             x +
-  //             ") != -1 || (datum." +
-  //             y +
-  //             " > highlight.data[0] && datum." +
-  //             y +
-  //             " < highlight.data[1]) || indexof(highlight.data,datum." +
-  //             color +
-  //             ") !== -1",
-  //           value: 1.0
-  //         },
-  //         { value: 0.4 }
-  //       ]
-  //     };
-  //   }
-  // }
-  // console.log("New Chart Specs:", spec);
   return spec;
 }
 
-export default addSignalToChartSpec;
+export function addSignalToMark(mark){
+  if (mark.type==="group"){// recursive 
+    if (mark.marks){
+      mark.marks.forEach(mark=>addSignalToMark(mark))// will this generalize to all charts? 
+    }
+    return;
+  }
+  mark.encode.update = {
+    ...mark.encode.update,
+    opacity:[
+      {
+        test:` 
+            highlight.enabled === false 
+            || indexof(highlight.data, datum[highlight.field])!=-1
+          `,
+        value: 1.0
+      },
+      {
+        value: 0.2
+      }
+    ]
+  }
+}
--- src/utils/extractChartFeatures.js
+++ src/utils/extractChartFeatures.js
@@ -55,7 +55,6 @@ export default async function (spec) {
                     value = isDate(value)?value:toString(value);
                     const type = isDate(value)? 'date':'string'
                     const field = searchFieldName(spec, scale.name);
-                    console.log('field', field);
                     featureMap[value] = {
                         value,
                         type,
@@ -67,78 +66,6 @@ export default async function (spec) {
         })
 
     console.log('features', Object.values(featureMap));
-
-
-    // console.log('viewdata', view.data(spec.data[0].name));
-    // // if (Array.isArray(data)){// always array if converted from vega-lite
-    //     await Promise.all(data.map(async d=>{
-    //         console.log(d);
-    //         if (d.url){
-    //             const ext = d.url.split('.').pop();
-    //             console.log('ext', ext);
-    //             if (ext==='json'){
-    //                 console.log('json');
-    //             }else if(ext==='csv'){
-    //                 console.log('csv');
-    //                 const values = await loader.load(d.url);
-    //                 // console.log('values', values);
-    //             }
-    //         }else{
-    //             console.log('inline')
-
-    //         }
-
-    //     }));
-    // }else {//if (typeof data === 'object')
-
-    //     if (data.url){
-    //         const ext = data.url.split('.').slice(-1);
-    //         if (ext==='json'){
-    //             console.log('json');
-    //         }else if(ext==='csv'){
-    //             console.log('csv');
-    //         }
-
-
-    //     }else{
-    //         console.log('inline')
-
-    //     }
-    // }
-    // console.log('return');
-
-
-    // if (spec.data.values){
-
-    // }else if (spec.data.url){
-
-    // }
-    // let features = [],
-    // scaleNames = [],
-    // fields = [];
-
-    // //extracting the name of all scales
-    // // console.log("spec:", spec);
-    // spec.scales.map(s => {
-    //     // scaleNames.push(s["name"]);
-    //     fields.push(s.domain.field);
-    // });
-    // // if (spec.data[0].url !== undefined) {
-    // //   const path = process.env.PUBLIC_URL + spec.data[0].url;
-    // //   //TODO: Handle the case where data is read from the URL
-    // //   return;
-    // // }
-
-    // fields.forEach(function (field) {
-    //     data[0].values.map(val => {
-    //         if (val[field] !== undefined) features.push(val[field].toString()); //FuzzySet expects a string value!
-    //     });
-    // });
-    // // Get Unique Elements in case of repetition
-    // features = [...new Set(features)];
-    // features = features.filter(f => {
-    //     return f !== undefined;
-    // });
     return Object.values(featureMap);
 };
 
