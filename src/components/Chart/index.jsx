345415f39251264d96a6ee3cb51c92adb72ecfef
Nam Wook Kim <nam.wook.kim@bc.edu>
2020-04-19 19:04:30 -0400

    remove Vega and changed the draggable styling

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,39 +1,54 @@
-import React, { useMemo,memo } from "react";
-import { Vega } from "react-vega";
+import React, { useMemo, memo, useRef, useState, useEffect } from "react";
+import vegaEmbed from 'vega-embed';
 import PropTypes from "prop-types";
 import css from './index.module.css';
-
+import Paper from '@material-ui/core/Paper';
 // See ChartBlock for a chart in the editor
-export function Chart(props){
+export function Chart(props) {
+  const chartEl = useRef(null);
+  // why we need to stringify and parse it back? needs further investigation
+  const spec = useMemo(() => JSON.parse(JSON.stringify(props.spec)), [props.spec]);
+  // const [view, setView] = useState(null);
+  
+  useEffect( ()=>{
+    vegaEmbed(chartEl.current, spec).then(result=>{
+      // setView(result.view);
+    });
+  },[]);// will run only once
 
-  function handleDragStart(e){
+
+  function handleDragStart(e) {
     e.dataTransfer.setData('chartId', props.id);
   }
 
-  function handleError(){
-    console.error(arguments);
+  const [hover, setHover] = useState(false);
+  function handleEnter(){
+    setHover(true);
   }
-
-  // why we need to stringify and parse it back? needs further investigation
-  const spec = useMemo(()=> JSON.parse(JSON.stringify(props.spec)),[props.spec]);
-
+  function handleLeave(){
+    setHover(false);
+  }
   return (
-    <div className={[css.container,
-      !props.inEditor?css.draggable:''].join(' ')} 
-      draggable={!props.inEditor} 
-      onDragStart = {handleDragStart}
+    <Paper className={css.container}
+      elevation={hover?2:0}>
+      <div
+        draggable={true}
+        onDragStart={handleDragStart}
+        onMouseEnter={handleEnter}
+        onMouseLeave={handleLeave}
       >
-        <Vega spec={ spec} onParseError={handleError} />
+        <div ref={chartEl} />
       </div>
+    </Paper>
   )
 }
 
 
 Chart.propTypes = {
-  id:PropTypes.any,
-  spec:PropTypes.object,
-  width:PropTypes.number,
-  height:PropTypes.number
+  id: PropTypes.any,
+  spec: PropTypes.object,
+  width: PropTypes.number,
+  height: PropTypes.number
 }
 
 export default memo(Chart);
