fd2fe7fa8fbea3e99a234bc6f17f69f6d09bbb21
Shahid Latif <shahid.latif@uni-due.de>
2020-05-07 09:30:42 +0200

    start implementing range selections using Wit.ai API

--- src/utils/findLinks.js
+++ src/utils/findLinks.js
@@ -1,12 +1,18 @@
 import FuzzySet from "fuzzyset.js";
 import splitTextIntoNWordsList from "./splitTextIntoNWordsList";
+import { Wit, log } from "node-wit";
 
 const MIN_MATCH_THRESHOLD = 0.8;
+const client = new Wit({
+    accessToken: "FFJCMCE6JAQ3CT52WH5YBFED5TKENKTI",
+    logger: new log.Logger(log.DEBUG), // optional
+});
 
 export default (charts, sentence) => {
     let links = [];
     charts.forEach(function (chart) {
         links = links.concat(findWordLink(chart, sentence).filter((link) => link !== null));
+        findPhraseLink(chart, sentence);
     });
     return links;
 };
@@ -15,7 +21,7 @@ export const findWordLink = (chart, sentence) => {
     let matches = [];
     chart.features.forEach(function (f) {
         const fs_res = fuzzyMatch(sentence.text, f.value); //returns a [score, word] pair!
-        console.log("FUZZY MATCH", fs_res);
+        // console.log("FUZZY MATCH", fs_res);
         // if (f.type === "string" && sentence.text.includes(f.value)) {
         if (f.type === "string" && fs_res[0] > MIN_MATCH_THRESHOLD) {
             matches.push({ userTyped: fs_res[1], matchedFeature: f });
@@ -48,4 +54,52 @@ function fuzzyMatch(sentence, word) {
             : splitTextIntoNWordsList(sentence, word.split(" ").length);
     let fs = FuzzySet(list);
     return fs.get(word) !== null ? fs.get(word).shift() : [0, ""];
+}
+
+function findPhraseLink(chart, sentence) {
+    let match = null;
+    chart.features.forEach(function (f) {
+        const fs_res = fuzzyMatch(sentence.text, f.field); //returns a [score, word] pair!
+        console.log("FUZZY MATCH", fs_res);
+        // TODO: Later check only if the f.type is a number!
+        if (f.type === "string" && fs_res[0] > MIN_MATCH_THRESHOLD) {
+            match = { userTyped: fs_res[1], matchedFeature: f };
+        }
+    });
+    //only send request to Wit.ai when we have a potential match!
+    if (match !== null) {
+        client
+            .message(sentence.text, {})
+            .then((response) => {
+                console.log("RESPONSE", response);
+                const entities = parseResponse(response);
+
+                const link = {
+                    text: match.userTyped,
+                    feature: match.matchedFeature, //information about how the link was found
+                    chartId: chart.id,
+                    active: false,
+                    type: entities.intent,
+                    data: [match.matchedFeature.field],
+                    startIndex: sentence.startIndex,
+                    endIndex: sentence.endIndex,
+                    sentence: sentence.text,
+                };
+                console.log("Created LINK", link);
+            })
+            .catch(console.error);
+    }
+}
+function parseResponse(response) {
+    let entities = null;
+    if (response.entities.hasOwnProperty("range_selection")) {
+        if (response.entities.range_selection.shift().confidence > 0.5) {
+            const intent = "range_selection";
+            const min = response.entities.min_number.shift().value;
+            const max = response.entities.number.shift().value;
+            entities = { intent: intent, min: min, max: max };
+        }
+    }
+    console.log("Parsed Response", entities);
+    return entities;
 }
