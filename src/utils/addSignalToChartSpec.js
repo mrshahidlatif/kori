bd98d7c58c94361fc1c57d236099cb1fda863523
Nam Wook Kim <namwkim85@gmail.com>
2020-09-04 14:36:47 -0400

    fix highlighting not working on map

--- src/utils/addSignalToChartSpec.js
+++ src/utils/addSignalToChartSpec.js
@@ -40,24 +40,16 @@ export function addSignalToMark(mark, highlight) {
     }
     const isMap = mark.type === "shape" && mark.style.includes("geoshape");
     const activePredicate = `highlight.enabled===true && (
-        (highlight.rangeField === '' && indexof(highlight.data, ${
-            isMap ? "datum.properties" : "datum"
-        }[highlight.field])!=-1) ||
-        (highlight.rangeField != '' && indexof(highlight.data, ${
-            isMap ? "datum.properties" : "datum"
-        }[highlight.field])!=-1 && (datum[highlight.rangeField] > highlight.rangeMin && datum[highlight.rangeField]<highlight.rangeMax)) ||
+        (highlight.rangeField === '' && (indexof(highlight.data, datum[highlight.field])!=-1 ${isMap? '|| indexof(highlight.data, datum.properties[highlight.field])!=-1)':')'}) ||
+        (highlight.rangeField != '' && (indexof(highlight.data, datum[highlight.field])!=-1 ${isMap? '|| indexof(highlight.data, datum.properties[highlight.field])!=-1)':')'} && (datum[highlight.rangeField] > highlight.rangeMin && datum[highlight.rangeField]<highlight.rangeMax)) ||
         (datum[highlight.field] > highlight.rangeMin && datum[highlight.field]<highlight.rangeMax) ||
         (datum[highlight.fieldX] > highlight.rangeX[0] && datum[highlight.fieldX] < highlight.rangeX[1] &&
             datum[highlight.fieldY] < highlight.rangeY[0] && datum[highlight.fieldY] > highlight.rangeY[1] )
     )`;
     //TODO: reduce redundant checking
     const inactivePredicate = `highlight.enabled===true && !(
-        (highlight.rangeField === '' && indexof(highlight.data, ${
-            isMap ? "datum.properties" : "datum"
-        }[highlight.field])!=-1) ||
-        (highlight.rangeField != '' && indexof(highlight.data, ${
-            isMap ? "datum.properties" : "datum"
-        }[highlight.field])!=-1 && (datum[highlight.rangeField] > highlight.rangeMin && datum[highlight.rangeField]<highlight.rangeMax))
+        (highlight.rangeField === '' && (indexof(highlight.data, datum[highlight.field])!=-1 ${isMap? '|| indexof(highlight.data, datum.properties[highlight.field])!=-1)':')'}) ||
+        (highlight.rangeField != '' && (indexof(highlight.data, datum[highlight.field])!=-1 ${isMap? '|| indexof(highlight.data, datum.properties[highlight.field])!=-1)':')'} && (datum[highlight.rangeField] > highlight.rangeMin && datum[highlight.rangeField]<highlight.rangeMax))
     )`;
     const oldProp = mark.encode.update[highlight.channel];
     const highlightProp = [
