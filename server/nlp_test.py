932c9b7f489c3ca9b18a5699ee589fd03faf3e15
Shahid Latif <shahid.latif@uni-due.de>
2021-03-11 13:22:02 +0100

    add nlp_test.py and related training data

---
+++ server/nlp_test.py
@@ -0,0 +1,139 @@
+import json
+import pandas as pd
+from nlp import *
+import math
+import spacy
+from spacy.matcher import Matcher
+
+THRESHOLD = 0.6
+nlp = spacy.load("en_core_web_md")
+
+
+def main():
+    print('-----Begin Testing NLP Module------')
+    overall_performance = [0, 0, 0]  # accurate, false_positives, missed
+    df = pd.read_excel('training_data/Training Dataset.xlsx')
+    for index, row in df.iterrows():
+        if(index > 5):
+            continue
+        sentence = row['Sentence']
+        chart_id = int(row['Chart ID'])
+        print('Processing Item # ', index, 'Chart ID # ',
+              chart_id, 'Sentence: ', sentence[0:80])
+        with open('training_data/features/' + str(chart_id) + '.json', 'r') as chart_feature_json:
+            chart_features = json.load(chart_feature_json)
+            # nlp methods accept array of charts
+            chart_properties = [{'properties': chart_features}]
+            # sentence_offset and block_key are only relevant for creating links in Draft.js!
+            links = find_links(chart_properties, sentence, 0, 'block_key')
+            matches = get_matches(links)
+            true_matches = get_true_matches(
+                row['Match Point Phrase'], row['Matched Range Phrase'], row['Matched Aggregated Phrase'])
+            performance = compute_performance(matches, true_matches)
+            print('Accurate/False Positives/Missed Instances', performance)
+            print('---')
+            overall_performance = [a + b for a,
+                                   b in zip(overall_performance, performance)]
+    print('Overall Performance Summary')
+    print('Accurate/False Positives/Missed Instances', overall_performance)
+    tp = overall_performance[0]
+    fp = overall_performance[1]
+    fn = overall_performance[2]
+    print('Accuracy:', round(tp/(tp+fp+fn)*100, 2))
+    print('Precision:', round(tp/(tp+fp)*100, 2))
+    print('Recall:', round(tp/(tp + fn)*100, 2))
+
+    print('-----End Testing NLP Module------')
+    test_interval_extraction()
+
+
+def test_interval_extraction():
+    count = 0
+    df = pd.read_excel('training_data/Interval Dataset.xlsx')
+    for index, row in df.iterrows():
+        sentence = row['Individual Sentences']
+        intervals = get_intervals(sentence)
+        if len(intervals) > 0:
+            count += 1
+            print('Success Count:', count, "/ 116")
+            print('sentence:', sentence[0:80])
+            print('Interval Matches:', intervals)
+
+
+def get_matches(links):
+    matches = []
+    for link in links:
+        matches.append(link.get('text'))
+    return matches
+
+
+def get_true_matches(point_matches, interval_matches, group_matches):
+    matches = [point_matches, interval_matches, group_matches]
+    true_matches = []
+    for match in matches:
+        if(isinstance(match, str)):
+            true_matches = true_matches + match.split('; ')
+    true_matches = [match for match in true_matches if match != '']
+    return true_matches
+
+
+def compute_performance(matches, true_matches):
+    true_positive = 0
+    false_positive = 0
+    false_negative = 0
+    for match in matches:
+        not_found = True
+        for true_match in true_matches:
+            if(is_partial_match(match, true_match)):
+                true_positive += 1
+                not_found = False
+                break
+        if(not_found):
+            false_positive += 1
+
+    for true_match in true_matches:
+        for match in matches:
+            not_found = False
+            if(not is_partial_match(match, true_match)):
+                not_found = True
+        if(not_found):
+            false_negative += 1
+    return [true_positive, false_positive, false_negative]
+
+
+def is_partial_match(a, b):
+    doc1 = nlp(a)
+    doc2 = nlp(b)
+    similarity = doc1.similarity(doc2)
+    return similarity > THRESHOLD
+
+
+def get_intervals(sentence):
+    matcher = Matcher(nlp.vocab)
+    pattern = [[{'POS': 'NUM'},
+                {'DEP': 'prep'},
+                {'POS': 'NUM'}],
+               [{'POS': 'NUM'},
+                {'POS': 'CCONJ'},
+                {'POS': 'NUM'}],
+               [{'POS': 'NUM'},
+                {'IS_PUNCT': True},
+                {'POS': 'NUM'}],
+               [{'ORTH': 'more'},
+                {'ORTH': 'than'},
+                {'POS': 'NUM'}],
+               [{'ORTH': 'less'},
+                {'ORTH': 'than'},
+                {'POS': 'NUM'}]]
+    matcher.add("Interval", pattern)
+    doc = nlp(sentence)
+    matches = matcher(doc)
+    # for match_id, start, end in matches:
+    #     string_id = nlp.vocab.strings[match_id]  # Get string representation
+    #     span = doc[start:end]  # The matched span
+    # print(match_id, string_id, start, end, span.text)
+    return matches
+
+
+if __name__ == "__main__":
+    main()
