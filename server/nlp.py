c93b53d53ea4f126b2e22eb759e54434a35cab1e
Shahid Latif <shahid.latif@uni-due.de>
2021-03-13 15:39:53 +0100

    handle single valued intervals

--- server/nlp.py
+++ server/nlp.py
@@ -59,31 +59,17 @@ def find_links(charts, sentence, sentence_offset, block_key):
             if result_fuzzy.get('similarity') > THRESHOLD:
                 print('axis', result_fuzzy)
                 interval_matches = get_intervals(sentence)
-                # print('Intervals', interval_matches)
+                print('Individual Intervals', interval_matches)
                 interval_links = combine_axis_interval(
                     sentence, result_fuzzy, interval_matches)
-                print('Interval Links', interval_links)
+                print('Axis-Interval Links', interval_links)
                 for link in interval_links:
                     interval_props = [{'min_val': link.get('min')}, {
                         'max_val': link.get('max')}]
                     range_link = create_link(link.get('text'), axis, chart.get('id'), axis.get(
                         'field'), link.get('offset'), sentence, sentence_offset, block_key, range_link_props=interval_props)
-                    print('Final Link', range_link)
+                    print('Final Links', link.get('text'))
                     links.append(range_link)
-                # wit_response = get_and_parse_wit_response(sentence)
-                # if(wit_response[0].get('min_body') != "" or wit_response[1].get('max_body') != ""):
-                #     entities = [result.get('matching_str'), wit_response[0].get(
-                #         'min_body'), wit_response[1].get('max_body')]
-                #     indices = [result.get('offset'), sentence.index(wit_response[0].get(
-                #         'min_body')), sentence.index(wit_response[1].get('max_body'))]
-                #     min_offset = min(i for i in indices if i > 0)
-                #     max_offset = max(i for i in indices)
-                #     max_offset = max_offset + \
-                #         len(entities[indices.index(max_offset)])
-                #     range_link_text = sentence[min_offset:max_offset]
-                #     range_link = create_link(range_link_text, axis, chart.get('id'), axis.get(
-                #         'field'), min_offset, sentence, sentence_offset, block_key, range_link_props=wit_response)
-                    # links.append(range_link)
     return links
 
 
@@ -93,17 +79,20 @@ def combine_axis_interval(sentence, axis_matches, interval_matches):
     links = []
     for axis in [axis_matches]:
         for interval in interval_matches:
+            # Sometimes range is defined with a hypen
+            # TODO See if we can combine two tokens (words) for computing shortest distance
+            # at the moment, first token of the interval is used.
+            term = re.split(' |-', interval.get('text'))[0]
             dist = compute_shortest_path(sentence, axis.get(
-                'matching_str'), interval.get('text').split(' ')[0])
+                'matching_str'), term)
             distances.append(dist)
+        # TODO how to handle if distances are equal?
         min_index = distances.index(min(distances))
         interval_end_offset = interval_matches[min_index].get(
             'offset') + len(interval_matches[min_index].get('text'))
         axis_offset = axis.get('offset')
-        # print('distances', distances)
         link_text = sentence[min(axis_offset, interval_end_offset): max(
             axis_offset, interval_end_offset)]
-        # print('link', link_text)
         link = interval_matches[min_index]
         link['text'] = link_text
         link['offset'] = sentence.find(link_text)
@@ -127,37 +116,15 @@ def compute_shortest_path(sentence, phrase_a, phrase_b):
     for a, b in edges:
         if a.find(phrase_a) > -1:
             phrase_a = a
+        if b.find(phrase_a) > -1:
+            phrase_a = b
+        if a.find(phrase_b) > -1:
+            phrase_b = a
         if b.find(phrase_b) > -1:
             phrase_b = b
-    # print('Node a & b', phrase_a, phrase_b)
-
     # https://networkx.github.io/documentation/networkx-1.10/reference/algorithms.shortest_paths.html
-    try:
-        dist = nx.shortest_path_length(graph, source=phrase_a, target=phrase_b)
-    except:
-        return 1000000
-    # print('Distance', dist)
+    dist = nx.shortest_path_length(graph, source=phrase_a, target=phrase_b)
     return dist
-
-
-def get_exact_matches(phrase, sentence):
-    matcher = PhraseMatcher(nlp.vocab)
-    # terms = ["Barack Obama", "Angela Merkel", "Washington, D.C."]
-    terms = [phrase.lower()]
-    if(phrase.find('_') > -1):
-        terms = [phrase.split('_')[0].lower()]
-    print('terms', terms)
-    # Only run nlp.make_doc to speed things up
-    patterns = [nlp.make_doc(text) for text in terms]
-    print('patterns', patterns)
-    matcher.add("TerminologyList", patterns)
-
-    doc = nlp(sentence.lower())
-    matches = matcher(doc)
-    print('Here comes the matches!')
-    for match_id, start, end in matches:
-        span = doc[start: end]
-        print(span.text)
 
 
 def fuzzy_substr_search(needle, hay):
@@ -199,23 +166,6 @@ def create_link(link_text, feature, chartId, val, offset, sentence, sentence_off
         link["rangeMin"] = range_link_props[0].get('min_val')
         link["rangeMax"] = range_link_props[1].get('max_val')
     return link
-
-
-def get_and_parse_wit_response(msg):
-    response = client.message(msg)
-    try:
-        max_val = response['entities']['wit$number:max'][0]['value']
-        max_body = response['entities']['wit$number:max'][0]['body']
-    except:
-        max_val = sys.float_info.max
-        max_body = ''
-    try:
-        min_val = response['entities']['wit$number:min'][0]['value']
-        min_body = response['entities']['wit$number:min'][0]['body']
-    except:
-        min_val = - sys.float_info.max
-        min_body = ''
-    return [{'min_body': min_body, 'min_val': min_val}, {'max_body': max_body, 'max_val': max_val}]
 
 
 def compute_word2vec_similarity(word, sentence):
@@ -284,6 +234,11 @@ def get_interval_extent(interval):
         # print(match_id, string_id, start, end, span.text)
         numbers.append(float(span.text))
     # TODO handle other cases with 1 value
+    if len(numbers) == 1:
+        if interval.find('more'):
+            return [min(numbers), sys.float_info.max]
+        if interval.find('less'):
+            return [- sys.float_info.max, max(numbers)]
     if len(numbers) == 2:
         return [min(numbers), max(numbers)]
     return [0, 0]
